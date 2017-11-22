d270a44997f8045b64748cdd48c97e8910e83d11
Ethan Fung <EthanEFung@gmail.com>
2017-11-22 14:51:22 -0800

    feat: drag and drop pieces

--- client/js/models.js
+++ client/js/models.js
@@ -1,17 +1,27 @@
 //TODO: create models
 
-function renderBoard(n) {
-  const board = [];
-  colIdx = "abcdefgh";
-  for (let i = 0; i < n; i++) {
-    const row = [];
-    let rowIdx = i;
-    for (let i = 0; i < n; i++) {
-      row.push({ piece: null, coord: colIdx[i] + (rowIdx + 1) });
-    }
-    board.unshift(row);
+class Board {
+  constructor(n) {
+    this.grid = this.render(n);
+    this.startGame(n);
   }
-  return board;
+  render(n) {
+    const board = [];
+    const colIdx = "abcdefgh";
+    for (let i = 0; i < n; i++) {
+      const row = [];
+      let rowIdx = i;
+      for (let i = 0; i < n; i++) {
+        row.push({ piece: null, coord: colIdx[i] + (rowIdx + 1) });
+      }
+      board.unshift(row);
+    }
+    return board;
+  }
+  startGame(n) {
+    this.grid[0].forEach(sq => (sq.piece = new Pawn("black", sq.coord)));
+    this.grid[n - 1].forEach(sq => (sq.piece = new Pawn("white", sq.coord)));
+  }
 }
 
 class Pawn {
@@ -19,6 +29,24 @@ class Pawn {
     this.side = side;
     this.coord = coord;
   }
+
+  /**
+   *
+   * @param {*} currentCoord
+   * @param {*} desiredCoord
+   * o: a pawn with a new coord
+   */
+  advance(currentCoord, desiredCoord) {
+    console.log("curr", currentCoord, "desired", desiredCoord);
+  }
+  capture(currentCoord, desiredCoord) {}
 }
 
-module.exports = { renderBoard };
+class Player {
+  constructor(winStatus, side) {
+    this.winStatus = winStatus;
+    this.side = side;
+  }
+}
+
+// module.exports = { renderBoard, Pawn, Player };
--- client/js/views.js
+++ client/js/views.js
@@ -1,5 +1,52 @@
 //TODO make views
+const board = new Board(3).grid;
+const $board = document.createElement("tbody");
+let desired;
+function renderView() {
+  if (document.getElementById("board")) {
+    document.getElementById("board").innerHTML = null;
+  }
+
+  $board.setAttribute("id", "board");
+  board.forEach((row, i) => {
+    const $row = document.createElement("tr");
+    $row.setAttribute("class", "row");
+    $row.setAttribute("row", i);
+    row.forEach(sq => {
+      const piece = sq.piece;
+      const $space = document.createElement("td");
+      $space.setAttribute("class", "space");
+      $space.setAttribute("coord", sq.coord);
+
+      $space.addEventListener("dragstart", function(e) {
+        console.log(piece.coord);
+      });
+      $space.addEventListener("dragover", function(e) {
+        e.preventDefault();
+        desired = e.target.getAttribute("coord");
+      });
+
+      $space.addEventListener("dragend", function(e) {
+        piece.advance(piece.coord, desired);
+      });
+
+      $space.addEventListener("drop", e => {});
+
+      if (piece) {
+        const $piece = document.createElement("span");
+        $piece.setAttribute("draggable", true);
+        $piece.innerHTML = piece.side;
+
+        $space.appendChild($piece);
+      }
+
+      $row.appendChild($space);
+    });
+    $board.appendChild($row);
+  });
+  document.body.appendChild($board);
+}
 
 window.onload = () => {
-  console.log("views connected");
+  renderView();
 };
