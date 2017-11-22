16409793fc3f03528dc34fdce27f2a290a1c35e2
Ethan Fung <EthanEFung@gmail.com>
2017-11-22 13:03:44 -0800

    feat: build out models and first test

--- client/js/models.js
+++ client/js/models.js
@@ -1,1 +1,24 @@
 //TODO: create models
+
+function renderBoard(n) {
+  const board = [];
+  colIdx = "abcdefgh";
+  for (let i = 0; i < n; i++) {
+    const row = [];
+    let rowIdx = i;
+    for (let i = 0; i < n; i++) {
+      row.push({ piece: null, coord: colIdx[i] + (rowIdx + 1) });
+    }
+    board.unshift(row);
+  }
+  return board;
+}
+
+class Pawn {
+  constructor(side, coord) {
+    this.side = side;
+    this.coord = coord;
+  }
+}
+
+module.exports = { renderBoard };
--- test/models.test.js
+++ test/models.test.js
@@ -1,1 +1,26 @@
 //TODO: create test suite
+const { renderBoard } = require("../client/js/models");
+
+describe("renderBoard", function() {
+  it("should render a board when a size is given", function() {
+    const actual = renderBoard(3);
+    const expected = [
+      [
+        { piece: null, coord: "a3" },
+        { piece: null, coord: "b3" },
+        { piece: null, coord: "c3" }
+      ],
+      [
+        { piece: null, coord: "a2" },
+        { piece: null, coord: "b2" },
+        { piece: null, coord: "c2" }
+      ],
+      [
+        { piece: null, coord: "a1" },
+        { piece: null, coord: "b1" },
+        { piece: null, coord: "c1" }
+      ]
+    ];
+    expect(actual).toEqual(expected);
+  });
+});
